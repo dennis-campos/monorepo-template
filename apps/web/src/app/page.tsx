b14ccbe7e766e74dc9826c88b5d5554da939035c
Dennis campos <97467610+dennis-campos@users.noreply.github.com>
2023-12-20 15:13:32 -0500

    add shadcn to test out

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Card, CardDescription } from '@repo/ui/card';
+
 export default function Home() {
   return (
     <main>
       <p className="bg-slate-400">fake bison template</p>
+      <Card className="w-[400px] p-8 bg-foreground text-white shadow-xl">
+        <CardDescription>hello world</CardDescription>
+      </Card>
     </main>
   );
 }
