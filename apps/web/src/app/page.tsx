58cb191b023f647bd5056d3e84a97b07b919c66c
Dennis campos <97467610+dennis-campos@users.noreply.github.com>
2023-12-21 09:58:04 -0500

    move config folders to tooling

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <p className="bg-slate-400">fake bison template</p>
-      <Card className="w-[400px] p-8 bg-foreground text-white shadow-xl">
+      <Card className="w-[400px] p-8 bg-green-500 text-white shadow-xl">
         <CardDescription>hello world</CardDescription>
       </Card>
     </main>
